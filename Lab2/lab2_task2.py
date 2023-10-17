aaaaa359c563ec6f54fd5c7cb518f36467c9ca79
favir0 <f4vir0@yandex.ru>
2023-10-18 01:44:28 +0700

    feat: tried to add multiprocessing but BRUH

--- Lab2/lab2_task2.py
+++ Lab2/lab2_task2.py
@@ -2,7 +2,9 @@ from keras.datasets import mnist
 import numpy as np
 import random
 from matplotlib import pyplot as plt
+from tqdm import tqdm
 import time
+from multiprocessing import Pool
 
 # Считывание данных
 def load_data():
@@ -26,6 +28,7 @@ class Neuron():
         self.value_to_find = value_to_find.copy()
         self.learning_rate = learning_rate
         self.inp = inp.copy()
+        self.epoch = 0
     
     def solve_specific_task(self, sideInput):
         curAnswer = 0
@@ -64,13 +67,17 @@ class Neuron():
  
     def calc_derivative(self):
         return 1
+    
+    def load_weights(self, new_weights):
+        self.weights = new_weights.copy()
 
-    def train_neuron(self, new_input = []):
-        if (len(new_input) != 0):
+    def train_neuron(self, new_input = [], new_answers = []):
+        if (len(new_input) != 0 or len(new_answers) != 0):
             print("before", len(self.inp))
             self.inp = new_input.copy()
+            self.value_to_find = new_answers.copy()
             print("after", len(self.inp))
-
+        epoch = 0
         complete_train = False
         max_point = 100
         time0 = time.perf_counter()
@@ -84,7 +91,9 @@ class Neuron():
                 if result != curAnswer:
                     allAnswersRight = False
                     self.recalc_weights(i, curAnswer)
+
             max_point -=1
+            epoch+=1
             if (max_point == 0):
                 print("Ошибка тренировки, бесконечный цикл")
                 break
@@ -117,14 +126,34 @@ def flattenNumberList(numberList):
             temp.extend(x if isinstance(x, list) else [x])
         return temp
 
-def continue_neuron_train(neuron, train_data, amount = 100):
+def continue_neuron_train(neuron, chosen_number, train_data, amount = 100):
     print("Amount of numbers to be included in train session: ", amount)
     flatten_train_images = []
-    for image in train_data["images"][0:amount]:
+    new_answers = []
+    for i, image in enumerate(train_data["images"][0:amount]):
         flatten_train_images.append(flattenNumberList(image))
+        if (train_data["labels"][i] == chosen_number):
+            new_answers.append(1)
+        else:
+            new_answers.append(0)
+    
+
     print(len(flatten_train_images))
-    neuron.train_neuron(flatten_train_images)
+    neuron.train_neuron(flatten_train_images, new_answers)
     return neuron
+
+def check_full_test(neuron, chosen_number, test_data):
+        successRate = 0
+        successCount = 0
+        totalCount = len(test_data["images"])
+        for (i, image) in enumerate(test_data["images"]):
+            flattenImage = flattenNumberList(image)
+            neuroResult = neuron.solve_specific_task(flattenImage)
+            if ((test_data["labels"][i] == chosen_number and neuroResult == 1) or
+                (test_data["labels"][i] != chosen_number and neuroResult == 0)):
+                successCount += 1
+        successRate = successCount / totalCount
+        return successRate
 
 def check_random_numbers(neuron, chosen_number,test_data):
         successRate = 0
@@ -160,41 +189,78 @@ def check_random_numbers(neuron, chosen_number,test_data):
             print(f"| Success rate = {successRate}% |")
             print( "------------------------------------")
 
+
+def train_neuron_for_digit(chosen_number, flatten_train_data_to_copy, lr, train_data_to_copy):
+    answers = []
+    print("abaras")
+    flatten_train_data = flatten_train_data_to_copy.copy()
+    train_data = train_data_to_copy.copy()
+    for label in train_data["labels"][0:len(flatten_train_data)]:
+        if (label == chosen_number):
+            answers.append(1)
+        else:
+            answers.append(0)
+    
+    weights = [random.random() for _ in flatten_train_data[0]]
+    neuron = Neuron(weights, answers, lr, flatten_train_data)
+    neuron.train_neuron()
+    return chosen_number, neuron
+
+
 def main():
     train_data, test_data = load_data()
     flattenNumbers = []
     weights = []
-
+    testing = False
+    if (testing):
     # Создаём тренировочную выборку по 1 экзепляру каждой цифры, переводим двумерные списки в одномерные
-    for i in range (10):
-        flattenNumbers.append(flattenNumberList((get_one_number(i, train_data))))
+        for i in range (10):
+            flattenNumbers.append(flattenNumberList((get_one_number(i, train_data))))
 
-    # Создаём случайные стартовые веса для нейрона
-    for i in range (len(flattenNumbers[0])):
-        weights.append(random.random())
+        # Создаём случайные стартовые веса для нейрона
+        for i in range (len(flattenNumbers[0])):
+            weights.append(random.random())
 
-    # Заполняем массив ответов, 1 у выбранной цифры и 0 у прочих  
-    chosen_number = 2  
-    answers = get_answers_list(chosen_number)
-    lr = 0.01
-    print(answers)
-    print("Learning rate: ", lr)
-    neuronchik = Neuron(weights, answers, lr, flattenNumbers)
-    
-    # Обучение нейрона
-    neuronchik.train_neuron()
+        # Заполняем массив ответов, 1 у выбранной цифры и 0 у прочих  
+        chosen_number = 2  
+        answers = get_answers_list(chosen_number)
+        lr = 0.01
+        print(answers)
+        print("Learning rate: ", lr)
+        neuronchik = Neuron(weights, answers, lr, flattenNumbers)
+        
+        # Обучение нейрона
+        neuronchik.train_neuron()
 
-    print("Проверка правильности обучения на исходных данных: ")
-    for i in range (10):
-        print ("Checking number ", i)
-        if (answers[i] == neuronchik.solve_task(i)):
-            print("Success")
-        else:
-            print("Error")
-    continue_neuron_train(neuronchik, train_data, amount=len(train_data["images"]))
-    check_random_numbers(neuronchik, chosen_number, test_data)
+        print("Проверка правильности обучения на исходных данных: ")
+        for i in range (10):
+            print ("Checking number ", i)
+            if (answers[i] == neuronchik.solve_task(i)):
+                print("Success")
+            else:
+                print("Error")
+        continue_neuron_train(neuronchik, chosen_number, train_data, amount=1000)
+        #check_random_numbers(neuronchik, chosen_number, test_data)
+        print(check_full_test(neuronchik, chosen_number, test_data)*100,"%")
+    else:
+        flatten_train_images = []
+        lr = 0.01
+        amount = 1000
+        for i, image in enumerate(train_data["images"][0:amount]):
+            flatten_train_images.append(flattenNumberList(image))
+        processes = []
+        results = []
+        temp_args = []
+        for digit in range(10):
+            temp_args.append([digit, flatten_train_images, lr, train_data])
+        pool = Pool(processes=10)
 
-
+        for result in pool.starmap(train_neuron_for_digit, temp_args):
+                results.append(result)
+        print(results)
+            
+            
+        
     
     
 if __name__ == "__main__":
